454cf60c35e51c7feb5121403030168ccc08e9af
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-12 12:45:28 -0600

    12:44PM

---
+++ Chapter2/src/MyInitials.java
@@ -0,0 +1,22 @@
+
+/*
+
+Program: ????.java          Last Date of this Revision: September 30, 2019
+
+Purpose: An application that uses nested for loops to output a table that diplays number in 
+ multiples of ten.
+
+Author: Your Name, 
+School: CHHS
+Course: Computer Programming ??
+ 
+
+*/
+public class MyInitials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter2/src/TicTacToeBoard.java
@@ -0,0 +1,25 @@
+
+/*
+
+Program: ????.java          Last Date of this Revision: September 30, 2019
+
+Purpose: An application that uses nested for loops to output a table that diplays number in 
+ multiples of ten.
+
+Author: Your Name, 
+School: CHHS
+Course: Computer Programming ??
+ 
+
+*/
+public class TicTacToeBoard {
+
+	public static void main(String[] args) {
+		
+		System.out.print("     |     |");
+		System.out.println("     |     |");
+		System.out.println("-----------------------------");
+		
+	}
+
+}
