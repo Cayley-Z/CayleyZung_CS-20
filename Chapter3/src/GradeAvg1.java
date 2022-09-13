fbd599439023fd7e15dab89052f7901b9df738f2
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-13 15:33:52 -0600

    3:33PM

---
+++ Chapter3/src/GradeAvg1.java
@@ -0,0 +1,22 @@
+
+/*
+
+Program: GradeAvg1.java          Last Date of this Revision: September 13, 2022
+
+Purpose: 
+
+Author: Cayley Zung, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+public class GradeAvg1 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
