a47d2bde334e7dac928291eb8b5cce48c9c8d0af
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-13 14:34:44 -0600

    2:34PM wowowoowowowwoooooooooo

---
+++ Chapter2/src/CRT.java
@@ -0,0 +1,68 @@
+
+public class CRT {
+
+	public static void main(String[] args) {
+		
+		/*
+			3. Write an appropriate comment block for the beginning of a program to describe an application that is intended to calculate test averages.
+
+				AverageScore.java
+				Program Purpose: Calculates test averages
+				@CayleyZ
+				Course: Computing Science 20
+				Date: September 13, 2022
+			
+			
+			
+			
+			4. Write an appropriate comment for describing a class that displays the average of a set of scores.
+			
+			// AverageScore class displays the average set of scores.
+			
+			I honestly don't think this can be simplified any further.
+			
+			
+			
+			
+			5. Write a statement that declares a package with the name gradeCalculator
+			
+			package gradeCalculator;
+			
+			
+			
+			
+			6. Write a statement that declares a class named AverageScore that is available to any code.
+			
+			public class AverageScore {
+				
+				public static void main(String[] args) {
+					
+					System.out.println("Sure, this is basic, and a good refresher, but really? Should I put ANYTHING inside this?");
+					System.out.println("I NEED SOMETHING TO LOOK AT BECAUSE I AM A WHOLE CHILD. EYEBALLS NEED CONSTANT ENTERTAINMENT.");
+					System.out.println("WHERE IS THING FOR ME TO LOOK AT. :sobbing_emoji: :sobbing_emoji: :sobbing_emoji:");
+					System.out.println("And before you ask, yes colons were intentional");
+					
+				}
+			
+			}
+			
+			
+			
+			
+			7. Explain the difference between source code and bytecode.
+			
+			Source code: Code typed by a programmer
+			Bytecode: Compiled source code
+			
+			
+			
+			
+			12. Explain the difference between the print() and println() methods.
+			
+			print() just outputs the thing straight up
+			println() outputs the thing on the next line
+		 */
+
+	}
+
+}
