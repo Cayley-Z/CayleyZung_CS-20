9b92439a43707e01c8784d2b555dc6d44c5505c8
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-13 14:56:47 -0600

    2:56PM

--- Chapter3/src/RectanglePerimeter.java
+++ Chapter3/src/RectanglePerimeter.java
@@ -1,8 +1,26 @@
 
+/*
+
+Program: RectanglePerimeter.java          Last Date of this Revision: September 13, 2022
+
+Purpose: Outputs total distance of a race
+
+Author: Cayley Zung, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
 public class RectanglePerimeter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Int
+		double First = 12.2;
+		double Sec = 10.6;
+		double Third = 5.8;
+		
+		System.out.println("The length of the race is" + First + Sec + Third + "km long.");
 
 	}
 
