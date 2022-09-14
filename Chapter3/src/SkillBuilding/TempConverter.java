4de3fc57b0edcafcb4aabb01596572f36468f6ac
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-14 13:25:32 -0600

    1:25PM

--- Chapter3/src/SkillBuilding/TempConverter.java
+++ Chapter3/src/SkillBuilding/TempConverter.java
@@ -21,13 +21,40 @@ public class TempConverter {
 		Scanner input = new Scanner(System.in);
 		
 		double fahren;
-		//
-		System.out.println("RACE LENGTH PROGRAM\n");
+		
+		System.out.println("FAHRENHEIT TO CELCIUS PROGRAM\n");
 		System.out.println("-----------------------------------------");
-		System.out.println("Input a WHOLE NUMBER distance for the FIRST segment (in km): \n");
-		fahren = input.nextInt();
-		double celcius = 5/9*(fahren - 32);
+		System.out.println("Input a temperature in FAHRENHEIGHT: \n");
+		fahren = input.nextDouble();
+		
+		double celcius = (fahren - 32)*5/9;
+		
+		System.out.println("\nYour temperature in CELCIUS is " + celcius + ".");
 
 	}
 
 }
+
+/* Screen Dump
+
+	FAHRENHEIT TO CELCIUS PROGRAM
+	
+	-----------------------------------------
+	Input a temperature in FAHRENHEIGHT: 
+	
+	66
+	
+	Your temperature in CELCIUS is 18.88888888888889.
+	
+	
+	
+	FAHRENHEIT TO CELCIUS PROGRAM
+	
+	-----------------------------------------
+	Input a temperature in FAHRENHEIGHT: 
+	
+	0
+	
+	Your temperature in CELCIUS is -17.77777777777778.
+
+ */
