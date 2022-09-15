b3d497c4d7341ec3a552407396fa5c9f9cae4bae
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-15 15:02:47 -0600

    3:03PM

--- Chapter3/src/SkillBuilding/Distance1.java
+++ Chapter3/src/SkillBuilding/Distance1.java
@@ -1,7 +1,5 @@
 package SkillBuilding;
 
-import java.text.DecimalFormat;
-
 /*
 
 Program: Distance1.java          Last Date of this Revision: September 15, 2022
@@ -14,6 +12,8 @@ Course: Computer Programming 20
  
 
 */
+import java.text.DecimalFormat;
+
 public class Distance1 {
 
 	public static void main(String[] args) {
