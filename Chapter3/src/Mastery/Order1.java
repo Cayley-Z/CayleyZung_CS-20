dbece4a65f2dc37a8c2984a4b45916418d71e72f
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-15 14:50:24 -0600

    2:50PM

--- Chapter3/src/Mastery/Order1.java
+++ Chapter3/src/Mastery/Order1.java
@@ -20,7 +20,33 @@ public class Order1 {
 		
 		Scanner input = new Scanner(System.in);
 		
+		int order;
+		double total;
+		double burger = 1.69;
+		double fries = 1.09;
+		double soda = 0.99;
+		double tax = 0.065;
 		
+		System.out.println("ORDERING PROGRAM\n");
+		System.out.println("-----------------------------------------");
+		System.out.println("\nPlease input how many BURGERS you'd like to order: \n");
+		order = input.nextInt();
+		total = order * burger;
+		
+		System.out.println("\nPlease input how many FRIES you'd like to order: \n");
+		order = input.nextInt();
+		total += order * fries;
+		
+		System.out.println("\nPlease input how many SODAS you'd like to order: \n");
+		order = input.nextInt();
+		total += order * soda;
+		
+		System.out.println("\nHere is your TOTAL BEFORE TAX: " + total);
+		tax = total * tax;
+		System.out.println("\nHere is your TAX: " + tax);
+		total = total + tax;
+		System.out.println("\nHere is your FINAL TOTAL: " + total);
+		System.out.println("\nThank you for ordering!");
 
 	}
 
