b3a3f4ea6349ecf7de2a52af64efd40b285e2ec3
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-15 14:40:24 -0600

    2:40PM

---
+++ Chapter3/src/Mastery/Order1.java
@@ -0,0 +1,27 @@
+package Mastery;
+
+/*
+
+Program: Order1.java          Last Date of this Revision: September 15, 2022
+
+Purpose: 
+
+Author: Cayley Zung, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+import java.util.Scanner;
+
+public class Order1 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+
+	}
+
+}
