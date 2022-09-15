6d57894eef5e30d975bf56770db3c83ed0f07052
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-15 15:02:39 -0600

    3:02PM

--- Chapter3/src/Mastery/Order1.java
+++ Chapter3/src/Mastery/Order1.java
@@ -13,12 +13,14 @@ Course: Computer Programming 20
 
 */
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Order1 {
 
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		DecimalFormat dc = new DecimalFormat("0.00");
 		
 		int order;
 		double total;
@@ -41,13 +43,63 @@ public class Order1 {
 		order = input.nextInt();
 		total += order * soda;
 		
-		System.out.println("\nHere is your TOTAL BEFORE TAX: " + total);
+		System.out.println("\nHere is your TOTAL BEFORE TAX: " + dc.format(total));
 		tax = total * tax;
-		System.out.println("\nHere is your TAX: " + tax);
+		System.out.println("Here is your TAX: " + dc.format(tax));
 		total = total + tax;
-		System.out.println("\nHere is your FINAL TOTAL: " + total);
+		System.out.println("Here is your FINAL TOTAL: " + dc.format(total));
 		System.out.println("\nThank you for ordering!");
 
 	}
 
 }
+
+/* Screen Dump
+
+ORDERING PROGRAM
+
+-----------------------------------------
+
+Please input how many BURGERS you'd like to order: 
+
+2
+
+Please input how many FRIES you'd like to order: 
+
+5
+
+Please input how many SODAS you'd like to order: 
+
+5
+
+Here is your TOTAL BEFORE TAX: 13.78
+Here is your TAX: 0.90
+Here is your FINAL TOTAL: 14.68
+
+Thank you for ordering!
+
+
+
+ORDERING PROGRAM
+
+-----------------------------------------
+
+Please input how many BURGERS you'd like to order: 
+
+14
+
+Please input how many FRIES you'd like to order: 
+
+300
+
+Please input how many SODAS you'd like to order: 
+
+24
+
+Here is your TOTAL BEFORE TAX: 374.42
+Here is your TAX: 24.34
+Here is your FINAL TOTAL: 398.76
+
+Thank you for ordering!
+
+ */
