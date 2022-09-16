9aa35c50364efe637b2622f59585ee815973258e
16279513 <16279513@L815WD220016.edu.cbe.ab.ca>
2022-09-16 10:59:45 -0600

    10:59PM

--- Chapter3/src/Mastery/Spending.java
+++ Chapter3/src/Mastery/Spending.java
@@ -53,16 +53,73 @@ public class Spending {
 		repair = (repair / total) * 100;
 		gas = (gas / total) * 100;
 		
-		System.out.print("\nCATEGORIES        BUDGET\n");
+		System.out.print("\n\nCATEGORIES         BUDGET\n");
 		
-		System.out.print("\nFood              " + dc.format(food));
-		System.out.print("\nFood              " + dc.format(food));
-		System.out.print("\nFood              " + dc.format(food));
-		System.out.print("\nFood              " + dc.format(food));
-		System.out.print("\nFood              " + dc.format(food));
-		System.out.print("\nFood              " + dc.format(food));
-		System.out.print("\nFood              " + dc.format(food));
+		System.out.print("\nFood               " + dc.format(food) + "%");
+		System.out.print("\nClothing           " + dc.format(cloth) + "%");
+		System.out.print("\nEntertainment      " + dc.format(enter) + "%");
+		System.out.print("\nRent               " + dc.format(rent) + "%");
+		System.out.print("\nRepairs            " + dc.format(repair) + "%");
+		System.out.print("\nGas/Transportation " + dc.format(gas) + "%");
 		
 	}
 
 }
+
+/* Screen Dump
+
+SPENDING PROGRAM
+
+-----------------------------------------
+
+Please how much you spend on FOOD: 350
+
+Please how much you spend on CLOTHING: 300
+
+Please how much you spend on ENTERTAINMENT: 200
+
+Please how much you spend on RENT: 1250
+
+Please how much you spend on REPAIRS: 0
+
+Please how much you spend on GAS/TRANSPORTATION: 0
+
+
+CATEGORIES         BUDGET
+
+Food               16.67%
+Clothing           14.29%
+Entertainment      9.52%
+Rent               59.52%
+Repairs            0.00%
+Gas/Transportation 0.00%
+
+
+
+SPENDING PROGRAM
+
+-----------------------------------------
+
+Please how much you spend on FOOD: 300
+
+Please how much you spend on CLOTHING: 40
+
+Please how much you spend on ENTERTAINMENT: 50
+
+Please how much you spend on RENT: 1500
+
+Please how much you spend on REPAIRS: 0
+
+Please how much you spend on GAS/TRANSPORTATION: 90
+
+
+CATEGORIES         BUDGET
+
+Food               15.15%
+Clothing           2.02%
+Entertainment      2.53%
+Rent               75.76%
+Repairs            0.00%
+Gas/Transportation 4.55%
+
+*/
